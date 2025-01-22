11f2a6332ca89e0398fe41ddf1b14906a7c536d3
rocio <rocioarnold330@gmail.com>
2025-01-22 19:41:08 -0300

    variables en js

---
+++ variables.js
@@ -0,0 +1,18 @@
+//MALA PRACTICA
+var personal = {
+    nombres: "Juan"
+    , apellidos: "Vera"
+    , anhos: 164
+};
+
+//buena practica
+let nombres = "Francisco Solano El Pollo Lopez"
+
+// constantes
+const nocambia = false;
+let costo_presupuesto = 63;
+
+console.log(typeof(personal), personal);
+console.log(typeof(nombres), nombres);
+console.log(typeof(nocambia), nocambia)
+console.log(typeof(costo_presupuesto), costo_presupuesto)
