60eef4bbc675de8c4103e46289d3525716124b5c
balayrocco <rocioarnold330@gmail.com>
2025-02-26 19:06:12 -0300

    Update aplicación-1.js

--- aplicación-1.js
+++ aplicación-1.js
@@ -1,2 +1,16 @@
-//prompt("Salúdame perro :)");
-SpeechRecognitionAlternative("Salúdame perro :)")
+documento.agregarEventListener(Contenido DOM cargado', función());
+    constante elemento_circulo = documento.obtenerElementoPorId("2d");
+    constante contexto = elemento_circulo.obtener contexto "2d";
+    contexto.comenzarPath();
+    contexto.arco(95,50,40,0.2*Matemáticas.P1);
+    contexto.tanque();
+
+    elemento_circulo.agregarEventListener('hacer clic', función()
+    Saludar();
+});
+
+
+
+función Saludar() {
+    alerta(Hiciste clic);
+}
